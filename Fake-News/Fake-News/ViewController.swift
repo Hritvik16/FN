6a1fd64aa2ad64e6974f7bb883435aa1b5c796ea
Hritvik16 <hvenkateshwa@umass.edu>
2018-09-05 12:24:53 -0400

    First time adding project

---
+++ Fake-News/Fake-News/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Fake-News
+//
+//  Created by Hritvik JV on 05/09/18.
+//  Copyright © 2018 Hritvik JV. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Fake-News/Fake-NewsTests/Fake_NewsTests.swift
@@ -0,0 +1,34 @@
+//
+//  Fake_NewsTests.swift
+//  Fake-NewsTests
+//
+//  Created by Hritvik JV on 05/09/18.
+//  Copyright © 2018 Hritvik JV. All rights reserved.
+//
+
+import XCTest
+@testable import Fake_News
+
+class Fake_NewsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
